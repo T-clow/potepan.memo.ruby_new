cfd9da6f56a9757e1ba2db27c608bcee2fe2c083
T-clow <144991160+T-clow@users.noreply.github.com>
2023-10-17 13:36:07 +0900

    Update memo.rb

--- memo.rb
+++ memo.rb
@@ -8,26 +8,23 @@ memo_type = gets.to_i
 # if文を使用して続きを作成していきましょう。
 # 「memo_type」の値（1 or 2）によって処理を分岐させていきましょう.
 if memo_type == 1
-  puts "拡張子を除いたファイル名を入力してください."
+  puts "拡張子を除いたファイル名を入力してください"
   file_name = gets.chomp + ".csv"
 
-  puts "新規でメモを作成します."
-  puts "メモの内容を入力してください:"
-  memo_content = gets.chomp
-
-  puts "終了後、Ctrl + Dを押します."
-
-  STDIN.read
+  puts "新規でメモを作成します"
+  puts "メモの内容を入力してください"
+  puts "終了時にCtrl + Dを押してください:"
+  memo_content = STDIN.read
 
   CSV.open(file_name, "w") do |csv|
-    csv << [file_name, memo_content]
+    csv << [memo_content]
   end
 
-  puts "メモが保存されました."
+  puts "メモが保存されました"
 end
 
 if memo_type == 2
-  puts "既存のメモを編集します."
+  puts "既存のメモを編集します"
 
   # 既存メモのファイル名を取得
   puts "編集したいメモのファイル名を入力してください:"
@@ -38,17 +35,16 @@ if memo_type == 2
     memos = CSV.read(file_name)
     puts "既存のメモ内容: #{memos[0][1]}"
 
-    puts "新しいメモの内容を入力してください:"
-    new_memo_content = gets.chomp
-    puts "終了後、Ctrl + Dを押します."
-    STDIN.read
+    puts "新しいメモの内容を入力してください"
+    puts "終了時にCtrl + Dを押してください:"
+    new_memo_content = STDIN.read
 
     # ファイルを上書きして新しいメモを保存
     CSV.open(file_name, "w") do |csv|
-      csv << [file_name, new_memo_content]
+      csv << [new_memo_content]
     end
-    puts "メモが編集され、保存されました."
+    puts "メモが編集され、保存されました"
   else
-    puts "指定したファイルが見つかりません."
+    puts "指定したファイルが見つかりません"
   end
 end
